0a91f56616a8a0a50274b4a04ae02a333ea0ef7b
kindlingscript <inflamedmuse@gmail.com>
2016-04-28 19:07:43 -0500

    Added jQuery for undoing action + style changes etc

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
-  console.log("Hello! I'm working!");
   var isClicked = false;
 
   $(".text-field").on("click", ".add-item" , function(e) {
     e.preventDefault();
     var input = $("#item-entered").val();
-    $("ul").append("<li class='added'><i class='fa-li fa fa-square-o'></i>" + input + "</li>");
+    if (input == "") {
+      alert("Please enter an item before submitting. Thanks! :)");
+    } else {
+      $("ul").append("<li class='added'><i class='fa-li fa fa-square-o'></i>" + input + "</li>");
+    }
     $("#item-entered").val("").focus();
   })
 
@@ -21,11 +24,17 @@ $(document).ready(function() {
 
   $(".fa-ul").on("click", ".added", function() {
     $(this).isClicked = true;
-    $(this).removeClass().addClass("to-delete");
     $(this).children("i").removeClass("fa-square-o").addClass("fa-trash");
+    $(this).removeClass().addClass("delete-or-undo center").append("<i class='fa-li fa fa-undo'></i>");
   })
 
-  $(".fa-ul").on("click", ".to-delete", function() {
-    (this).remove();
+  $(".fa-ul").on("click", ".delete-or-undo > .fa-undo", function() {
+    $(this).parent(".delete-or-undo").children("i").removeClass("fa-trash").addClass("fa-square-o");
+    $(this).parent(".delete-or-undo").removeClass().addClass("added");
+    $(this).remove(".fa-undo");
+  })
+
+  $(".fa-ul").on("click", ".delete-or-undo > .fa-trash", function() {
+    $(this).parent(".delete-or-undo").remove();
   })
 });
