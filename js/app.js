d0a08e4fce88f4a639d94fc09694ad9e83d2fba6
kindlingscript <inflamedmuse@gmail.com>
2016-05-04 03:05:25 -0500

    Instructions .show() jQuery and autocomplete added

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
       $("ul").append("<li class='added'><i class='fa-li fa fa-square-o'></i>" + input + "</li>");
     }
     $("#item-entered").val("").focus();
+    $(".instructions").show().css("display", "inline-block");
   })
 
   $(".fa-ul").on("mouseover", ".added" , function() {
@@ -37,4 +38,42 @@ $(document).ready(function() {
   $(".fa-ul").on("click", ".delete-or-undo > .fa-trash", function() {
     $(this).parent(".delete-or-undo").remove();
   })
+
+  var shoppingList = [
+    { value: 'bananas'},
+    { value: 'apples'},
+    { value: 'grapes'},
+    { value: 'oranges'},
+    { value: 'orange juice'},
+    { value: 'eggs'},
+    { value: 'coffee'},
+    { value: 'ketchup'},
+    { value: 'butter'},
+    { value: 'water'},
+    { value: 'milk'},
+    { value: 'cookies'},
+    { value: 'rolls'},
+    { value: 'lemons'},
+    { value: 'lemonade'},
+    { value: 'peanuts'},
+    { value: 'cereal'},
+    { value: 'shampoo'},
+    { value: 'toothpaste'},
+    { value: 'paper towels'},
+    { value: 'toilet paper'},
+    { value: 'potato chips'},
+    { value: 'bread'},
+    { value: 'deodorant'},
+    { value: 'pencils'},
+    { value: 'paper'}
+  ];
+  
+  // setup autocomplete function
+  $("#item-entered").autocomplete({
+    lookup: shoppingList,
+    onSelect: function (suggestion) {
+      var thehtml = "<strong>Item:</strong> " + suggestion.value;
+      $("#outputcontent").html(thehtml);
+    }
+  });
 });
